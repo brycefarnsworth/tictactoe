96b1dee4b5b0c6984a1a10d659fc585f9abb30d5
Bryce Farnsworth <bryce.farnsworth@gmail.com>
2015-08-11 11:16:22 -0700

    Reduced gamestate to just the board. Added score() method.

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -28,23 +28,23 @@ class Game(object):
 		gamestate_board = []
 		for i in range(3):
 			gamestate_board.append(list(self.board[i]))
-		return (gamestate_board, self.player)
+		return gamestate_board
 		
 	def empty_spaces(self, gamestate):
 		for i in range(3):
 			for j in range(3):
-				if gamestate[0][i][j] == " ":
+				if gamestate[i][j] == " ":
 					return True
 		return False
 	
 	def get_winner(self, gamestate):
 		lines = []
-		for row in gamestate[0]:
+		for row in gamestate:
 			lines.append(row)
 		for i in range(3):
-			lines.append([gamestate[0][0][i], gamestate[0][1][i], gamestate[0][2][i]])
-		lines.append([gamestate[0][0][0], gamestate[0][1][1], gamestate[0][2][2]])
-		lines.append([gamestate[0][2][0], gamestate[0][1][1], gamestate[0][0][2]])
+			lines.append([gamestate[0][i], gamestate[1][i], gamestate[2][i]])
+		lines.append([gamestate[0][0], gamestate[1][1], gamestate[2][2]])
+		lines.append([gamestate[2][0], gamestate[1][1], gamestate[0][2]])
 		if ["X", "X", "X"] in lines:
 			return "X"
 		elif ["O", "O", "O"] in lines:
@@ -62,25 +62,32 @@ class Game(object):
 		legal_moves = []
 		for i in range(3):
 			for j in range(3):
-				if gamestate[0][i][j] == " ":
+				if gamestate[i][j] == " ":
 					legal_moves.append((i, j))
 		return legal_moves
 		
 	def move(self, row, col):
-		if (row, col) in self.get_legal_moves((self.board, self.player)):
+		if (row, col) in self.get_legal_moves(self.board):
 			self.board[row][col] = PLAYER[self.player]
 			self.player = (self.player + 1) % 2
+			
+	def score(self, gamestate, player):
+		if self.get_winner(gamestate) == PLAYER[player]:
+			return 10
+		elif self.get_winner(gamestate) == PLAYER[(player + 1) % 2]
+			return -10
+		return 0
 	
 while True:
 	g = Game()
-	while not g.game_over((g.board, g.player)):
+	while not g.game_over(g.board):
 		g.print_board()
 		row = int(raw_input())
 		col = int(raw_input())
 		g.move(row, col)
 	g.print_board()
-	if g.get_winner((g.board, g.player)) != "D":
-		print "%c Wins!" % g.get_winner((g.board, g.player))
+	if g.get_winner(g.board) != "D":
+		print "%c Wins!" % g.get_winner(g.board)
 	else:
 		print "Draw!"
 	g.reset()
